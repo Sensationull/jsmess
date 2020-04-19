f6b1cc3253ca5d8007e0ebb76f500fcfdde93fa4
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-04-19 13:45:19 -0700

    added timer, not completely functional however

--- microwave/index.js
+++ microwave/index.js
@@ -8,4 +8,17 @@ function showTime() {
   setInterval(getTime, 1000);
 }
 
+/* How do you set the timer so that a minute in time (<- important) is displayed, but doesn't start until another button is pressed */
+let minute = setInterval(() => {
+  let display = document.querySelector('.display');
+  let date = new Date();
+  let time = (60 - date.getSeconds());
+  display.innerHTML = time
+  if (time <= 10) {
+    clearInterval(minute);
+    showTime();
+  } 
+}, 1000)
+
+
 showTime()
