be90b5decd46ed988485ee68b4d8bb0e26cc670e
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-05-11 23:31:40 -0700

    created mammals excercise

---
+++ mammals/index.js
@@ -0,0 +1,10 @@
+function getMammals() {
+  let allAnimals = document.querySelector('.all-animals');
+  let animals = allAnimals.querySelectorAll('div.animal')
+  for (let item of animals.values()) {
+    
+  }
+  
+}
+
+getMammals();
