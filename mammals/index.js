b17d8e12a7610cbd525cd4b05f4cb68f49ef4ffd
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-05-12 09:50:55 -0700

    created solution for accessing proper dom nodes

--- mammals/index.js
+++ mammals/index.js
@@ -1,10 +1,12 @@
 function getMammals() {
   let allAnimals = document.querySelector('.all-animals');
   let animals = allAnimals.querySelectorAll('div.animal')
+  let mammals = document.getElementById('only-mammals');
   for (let item of animals.values()) {
-    
+    if(item.lastElementChild.firstElementChild.textContent === 'true') {
+      mammals.appendChild(item);
+    }
   }
-  
 }
 
 getMammals();
