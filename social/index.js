58179d306c57ebca28c3cad9c1bdcdd1ba59841e
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-05-12 22:09:29 -0700

    added complete functionality to request ten items and style their names and picture

--- social/index.js
+++ social/index.js
@@ -1,12 +1,16 @@
 function getInfo() {
-  fetch('https://randomuser.me/api')
+  fetch('https://randomuser.me/api/?results=10')
     .then(function(res) {
       // console.log('success', res);
       return res.json();
     })
     .then(function(data) {
       let results = data.results
-      console.log(data.results, 'and the card ', displayInfo(results))
+      for (let item of results) {
+        displayInfo(item);
+        // Would it be better for this looping to happen outside of the promise?
+        // How would we refactor this to async/await?
+      }
     })
     .catch(function(err){
       console.log('Something is wrong', err);
@@ -14,17 +18,37 @@ function getInfo() {
 }
 
 function displayInfo(info) {
-  let card = document.querySelector('.card');
-  let properName = formatName(info)
-  card.textContent = properName
-  return properName;
+  // creates a card and appends it to the main
+  let main = document.querySelector('.main');
+  // Select the container that the info will live in
+  const card = document.createElement('div');
+  // create the card that will hold the image and name
+  const name = document.createElement('span');
+  // create the name container
+  const properName = formatName(info)
+  // Format the name coming in from the api call
+  name.textContent = properName
+  // place it into the text portion for the html element
+  name.classList.add('name');
+  // add the class of name to the element to add styles
+  const image = document.createElement('img');
+  //  create the image element
+  image.src = info.picture.thumbnail;
+  // set the source of the image's image from the api call
+  image.classList.add('image');
+  // append the image and the name to the card, as they are the children
+  card.appendChild(image);
+  card.appendChild(name);
+  //  style the card by adding the class
+  card.classList.add('card');
+  // finally add the card to main
+  main.appendChild(card);
 }
 
 function formatName(object) {
-  let title = object[0].name.title
-  let first = object[0].name.first
-  let last = object[0].name.last
-  return `${title} ${first} ${last}`
+  let first = object.name.first
+  let last = object.name.last
+  return `${first} ${last}`
 }
 
 getInfo();
