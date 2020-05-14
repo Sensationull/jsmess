9d7acb51462fab1f08d6649178a21768a58b2c67
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-05-13 20:40:06 -0700

    reformatted code to pull separate actions into separate functions asynchronously

--- social/index.js
+++ social/index.js
@@ -4,14 +4,11 @@ async function getInfo() {
       return res.json();
     })
     .then(function(data) {
-      console.log(data);
-      let results = data.results
-      for (let item of results) {
-        displayInfo(item);
-        // Would it be better for this looping to happen outside of the promise?
-        // How would we refactor this to async/await?
-      }
-      // return results
+      // let results = data.results
+      // for (let item of results) {
+      //   displayInfo(item);
+      // }
+      return data.results
     })
     .catch(function(err){
       console.log('Something is wrong', err);
@@ -19,15 +16,51 @@ async function getInfo() {
     return result;
 }
 
+/* async functions, written synchronously still accomplish the same job, is there any upside to writing them in this manner? */
+
 async function testing() {  
   console.log(await getInfo())
+  return await getInfo()
 }
 
-// testing();
+async function cycleData(data) {
+  let result = await data
+  result.forEach(item => displayInfo(item));
+}
+
+cycleData(testing());
+
+/**
+ * async function getInfo() {
+  let result = await fetch('https://randomuser.me/api/?results=10')
+    .then(function(res) {
+      return res.json();
+    })
+    // .then(function(data) {
+    //   console.log(data);
+    //   let results = data.results
+    //   // for (let item of results) {
+    //   //   displayInfo(item);
+    //   //   // Would it be better for this looping to happen outside of the promise?
+    //   //   // How would we refactor this to async/await?
+    //   // }
+    //   return results
+    // })
+    .then(info => info.results)
+    .catch(function(err){
+      console.log('Something is wrong', err);
+    })
+    return result;
+}
+(async () => {  
+  console.log(await getInfo())
+})()
+ */
 
 
-let people = getInfo();
-console.log(people)
+
+// let people = getInfo();
+// console.log(people)
 
 function displayInfo(info) {
   // creates a card and appends it to the main
@@ -45,6 +78,10 @@ function displayInfo(info) {
   // add the class of name to the element to add styles
   const image = document.createElement('img');
   //  create the image element
+  //  when dealing with larger requests or slower responses from the server use image onload
+  // image.onload = () => {
+  //   image.classList.toggle('fade-in');
+  // };
   image.src = info.picture.large;
   // set the source of the image's image from the api call
   image.classList.add('image');
@@ -62,10 +99,6 @@ function formatName(object) {
   let first = object.name.first
   let last = object.name.last
   return `${first} ${last}`
-}
-
-function cycleData(results) {
-  results.forEach(item => displayInfo(item));
 }
 
 // async function fetchInfo() {
