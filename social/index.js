700fa6e4935276a6fb166d864c4c269c227cec1e
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-05-13 18:54:54 -0700

    edited file to take in async function that returns the value synchronously

--- social/index.js
+++ social/index.js
@@ -1,21 +1,33 @@
-function getInfo() {
-  fetch('https://randomuser.me/api/?results=10')
+async function getInfo() {
+  let result = fetch('https://randomuser.me/api/?results=10')
     .then(function(res) {
-      // console.log('success', res);
       return res.json();
     })
     .then(function(data) {
+      console.log(data);
       let results = data.results
       for (let item of results) {
         displayInfo(item);
         // Would it be better for this looping to happen outside of the promise?
         // How would we refactor this to async/await?
       }
+      // return results
     })
     .catch(function(err){
       console.log('Something is wrong', err);
     })
+    return result;
 }
+
+async function testing() {  
+  console.log(await getInfo())
+}
+
+// testing();
+
+
+let people = getInfo();
+console.log(people)
 
 function displayInfo(info) {
   // creates a card and appends it to the main
@@ -33,7 +45,7 @@ function displayInfo(info) {
   // add the class of name to the element to add styles
   const image = document.createElement('img');
   //  create the image element
-  image.src = info.picture.thumbnail;
+  image.src = info.picture.large;
   // set the source of the image's image from the api call
   image.classList.add('image');
   // append the image and the name to the card, as they are the children
@@ -43,6 +55,7 @@ function displayInfo(info) {
   card.classList.add('card');
   // finally add the card to main
   main.appendChild(card);
+  card.classList.toggle('fade-in')
 }
 
 function formatName(object) {
@@ -51,4 +64,18 @@ function formatName(object) {
   return `${first} ${last}`
 }
 
-getInfo();
+function cycleData(results) {
+  results.forEach(item => displayInfo(item));
+}
+
+// async function fetchInfo() {
+//   try {
+//     const res = await fetch('https://randomuser.me/api/?results=10');
+//     // You can only consume the res.json once, but you can clone it
+//     // let copy = await res.clone().json()
+//     // let data = await res.json();
+//     return res.json();
+//   } catch (err) {
+//     console.log('Ooops', err);
+//   }
+// }
