25552774873c001e6a2f17f2b6bfd57d05814d1b
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-05-28 15:40:21 -0700

    added reset button to bring cards back, but animations now misfiring

--- redux/index.js
+++ redux/index.js
@@ -29,6 +29,21 @@ function eachNode () {
 
 eachNode();
 
+function showAllCards() {
+  let cards = document.querySelectorAll('.content')
+  cards.forEach(card => {
+    if (card.classList.contains('slide-left')) {
+      card.classList.toggle('slide-left');
+    } else {
+      card.style.display = 'flex';
+    }
+  });
+}
+
+/**
+ * You can use an animation to change the colors of a component, but it might be better to leave that as a transition property, because if you add animation event listeners on top of it, JS won't know which animation to listen to. right now I have a color change animation, a slide animation, and an event listener for 'animationend', which fires regardless of whether or not its the animation I want to fire
+ */
+
 // let el = document.querySelector('.content');
 // el.addEventListener('mouseover',fadeAway, false);
 // el.addEventListener('mouseout',fadeAway, false);
