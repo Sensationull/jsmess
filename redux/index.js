376183d01552d55d0c6af2e1802e6273f7506b97
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-05-28 21:54:12 -0700

    removed functions for changing color and nested event listeners, added notes, and event listener for reset button

--- redux/index.js
+++ redux/index.js
@@ -1,29 +1,14 @@
 /*on click, fade away and remove from DOM*/
-function slideAway () {
-  let content = document.querySelector('.content-1');
-  content.classList.toggle('slide-left');
-}
-
-function fadeAway () {
-  let allContent = document.querySelector('.content');
-  allContent.classList.toggle('shade');
-}
 
 function eachNode () {
   let allContent = document.querySelectorAll('.content');
   allContent.forEach(node => {
-    node.addEventListener('mouseover',() => {
-      node.classList.toggle('shade')
-    }, false);
-    node.addEventListener('mouseout', () => {
-      node.classList.toggle('shade');
-    }, false);
     node.addEventListener('click', () => {
       node.classList.toggle('slide-left');
-      node.addEventListener('animationend', ()=> {
-        node.style.display = 'none';
-      }, false)
     }, false);
+    node.addEventListener('animationend', ()=> {
+      node.style.display = 'none';
+    }, false)
   });
 }
 
@@ -39,6 +24,11 @@ function showAllCards() {
     }
   });
 }
+
+const resetButton = document.querySelector('.reset');
+resetButton.addEventListener('click', showAllCards, false);
+
+
 
 /**
  * You can use an animation to change the colors of a component, but it might be better to leave that as a transition property, because if you add animation event listeners on top of it, JS won't know which animation to listen to. right now I have a color change animation, a slide animation, and an event listener for 'animationend', which fires regardless of whether or not its the animation I want to fire
