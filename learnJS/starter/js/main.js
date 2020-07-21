0852ae037e63956eb534622013b0983307c67267
sensationull <andrewjohnso17@gmail.com>
2020-07-21 14:14:58 -0700

    added javascript to slide canvas and containers

--- learnJS/starter/js/main.js
+++ learnJS/starter/js/main.js
@@ -1,1 +1,8 @@
-// Start writing JavaScript here!
+const button = document.querySelector('.menu-button');
+const body = document.body;
+button.addEventListener('click', toggleMenu);
+
+
+function toggleMenu() {
+	body.classList.toggle('menu-is-open');
+}
