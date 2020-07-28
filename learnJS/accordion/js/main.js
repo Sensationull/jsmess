7233912dbe4788c315fdf9d9597f51f080c8a4dc
sensationull <andrewjohnso17@gmail.com>
2020-07-28 09:32:43 -0700

    added refactored final solution

--- learnJS/accordion/js/main.js
+++ learnJS/accordion/js/main.js
@@ -1,36 +1,36 @@
 // Start writing JavaScript here!
 // const openButtons = document.querySelectorAll('.accordion__header');
-const cheese = document.querySelector('[data-theme="cheese"]');
+// const cheese = document.querySelector('[data-theme="cheese"]');
 
-const showCheeseContent = () => {
-	cheese.classList.toggle('is-open');
-};
+// const showCheeseContent = () => {
+// 	cheese.classList.toggle('is-open');
+// };
 
-cheese.addEventListener('click', showCheeseContent);
+// cheese.addEventListener('click', showCheeseContent);
 
-const lettuce = document.querySelector('[data-theme="lettuce"]');
+// const lettuce = document.querySelector('[data-theme="lettuce"]');
 
-const showLettuceContent = () => {
-	lettuce.classList.toggle('is-open');
-};
+// const showLettuceContent = () => {
+// 	lettuce.classList.toggle('is-open');
+// };
 
-lettuce.addEventListener('click', showLettuceContent);
+// lettuce.addEventListener('click', showLettuceContent);
 
-const tomato = document.querySelector('[data-theme="tomato"]');
+// const tomato = document.querySelector('[data-theme="tomato"]');
 
-const showTomatoContent = () => {
-	tomato.classList.toggle('is-open');
-};
+// const showTomatoContent = () => {
+// 	tomato.classList.toggle('is-open');
+// };
 
-tomato.addEventListener('click', showTomatoContent);
+// tomato.addEventListener('click', showTomatoContent);
 
-const beef = document.querySelector('[data-theme="beef"]');
+// const beef = document.querySelector('[data-theme="beef"]');
 
-const showBeefContent = () => {
-	beef.classList.toggle('is-open');
-};
+// const showBeefContent = () => {
+// 	beef.classList.toggle('is-open');
+// };
 
-beef.addEventListener('click', showBeefContent);
+// beef.addEventListener('click', showBeefContent);
 
 
 // for (let button of openButtons) {
@@ -42,3 +42,14 @@ beef.addEventListener('click', showBeefContent);
   put an event listener on each button that grabs that specific 
   button's content and adds the is open class to that specific content
 */
+
+const accordions = Array.from(document.querySelectorAll('.accordion'));
+
+accordions.forEach( accordion => {
+	const accordionHeader = accordion.querySelector('.accordion__header');
+	/* each element has a query selector inside it to find the first class within itself that matches the class given */
+	accordionHeader.addEventListener('click', (event) => {
+		accordion.classList.toggle('is-open');
+		console.log(event);
+	});
+});
