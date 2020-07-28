482b8efcee3332662716f5909a78039529033eb4
sensationull <andrewjohnso17@gmail.com>
2020-07-28 09:21:20 -0700

    edited to get individual accordions, still refactoring

--- learnJS/accordion/js/main.js
+++ learnJS/accordion/js/main.js
@@ -1,9 +1,29 @@
 // Start writing JavaScript here!
-const openButton = document.querySelector('.accordion__header');
+// const openButtons = document.querySelectorAll('.accordion__header');
+const cheese = document.querySelector('[data-theme="cheese"]');
 
-const openAccordion = () => {
-	const accordion = document.querySelector('.accordion');
-	accordion.classList.toggle('is-open');
+const showCheeseContent = () => {
+	cheese.classList.toggle('is-open');
 };
 
-openButton.addEventListener('click', openAccordion);
+cheese.addEventListener('click', showCheeseContent);
+
+const lettuce = document.querySelector('[data-theme="lettuce"]');
+
+const showLettuceContent = () => {
+	lettuce.classList.toggle('is-open');
+};
+
+lettuce.addEventListener('click', showLettuceContent);
+
+
+
+// for (let button of openButtons) {
+// 	button.addEventListener('click', openAccordion);
+// }
+
+/*
+  Select All the accordion headers
+  put an event listener on each button that grabs that specific 
+  button's content and adds the is open class to that specific content
+*/
