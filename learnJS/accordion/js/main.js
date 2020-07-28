5589de7c3f59d8d8fc9156b554879b558f92b51a
sensationull <andrewjohnso17@gmail.com>
2020-07-28 09:04:59 -0700

    added init solution for opening one accordion

--- learnJS/accordion/js/main.js
+++ learnJS/accordion/js/main.js
@@ -1,1 +1,9 @@
 // Start writing JavaScript here!
+const openButton = document.querySelector('.accordion__header');
+
+const openAccordion = () => {
+	const accordion = document.querySelector('.accordion');
+	accordion.classList.toggle('is-open');
+};
+
+openButton.addEventListener('click', openAccordion);
