bba17a8f7ecd5cfd7b9567eac7b5227a599cddef
sensationull <andrewjohnso17@gmail.com>
2020-08-05 12:51:41 -0700

    added functionality for dots of carousel to toggle currently selected image

--- learnJS/carousel/js/main.js
+++ learnJS/carousel/js/main.js
@@ -2,7 +2,9 @@ const carousel = document.querySelector('.carousel');
 const previousButton = document.querySelector('.previous-button');
 const nextButton = document.querySelector('.next-button');
 const contents = document.querySelector('.carousel__contents');
-
+const dots = [...document.querySelectorAll('.carousel__dot')];
+const slides = [...document.querySelectorAll('.carousel__slide')];
+const dotsContainer = carousel.querySelector('.carousel__dots');
 
 nextButton.addEventListener('click', event => {
 	const currentSlide = contents.querySelector('.is-selected');
@@ -13,6 +15,10 @@ nextButton.addEventListener('click', event => {
 		currentSlide.classList.toggle('is-selected');
 		nextSlide.classList.toggle('is-selected');
 		nextSlide.nextElementSibling ? null : nextButton.setAttribute('hidden', true);
+		const currentDot = dotsContainer.querySelector('.is-selected');
+		const nextDot = currentDot.nextElementSibling;
+		currentDot.classList.remove('is-selected');
+		nextDot.classList.add('is-selected');
 	} 
 	previousButton.removeAttribute('hidden');
 });
@@ -26,6 +32,40 @@ previousButton.addEventListener('click', event => {
 		currentSlide.classList.toggle('is-selected');
 		previousSlide.classList.toggle('is-selected');
 		previousSlide.previousElementSibling ? null : previousButton.setAttribute('hidden', true);
+		const currentDot = dotsContainer.querySelector('.is-selected');
+		const prevDot = currentDot.previousElementSibling;
+		currentDot.classList.remove('is-selected');
+		prevDot.classList.add('is-selected');
 	}
 	nextButton.removeAttribute('hidden');
 });
+
+dots.forEach(dot => dot.addEventListener('click', event => {
+	let clickedIndex;
+  
+	for (let i = 0; i < dots.length; i++) {
+		if (dots[i] === dot) {
+			clickedIndex = i;
+		}
+	}
+	const slideToShow = slides[clickedIndex];
+	const destination = getComputedStyle(slideToShow).left;
+	contents.style.left = `-${destination}`;
+	let currentSlide = contents.querySelector('.is-selected');
+	currentSlide.classList.remove('is-selected');
+	slideToShow.classList.add('is-selected');
+  
+	dots.forEach(d => { d.classList.remove('is-selected');});
+	dot.classList.add('is-selected');
+  
+	if (clickedIndex === 0) {
+		previousButton.setAttribute('hidden', true);
+		nextButton.removeAttribute('hidden');
+	} else if (clickedIndex === dots.length -1) {
+		nextButton.setAttribute('hidden', true);
+		previousButton.removeAttribute('hidden');
+	} else {
+		nextButton.removeAttribute('hidden');
+		previousButton.removeAttribute('hidden'); 
+	}
+}));
