71f25dfe2124b745131ae64fcc8ec97e5064e9f9
sensationull <andrewjohnso17@gmail.com>
2020-08-05 11:43:18 -0700

    added basic structure and js to create working carousel

--- learnJS/carousel/js/main.js
+++ learnJS/carousel/js/main.js
@@ -1,1 +1,31 @@
-// Start writing JavaScript here!
+const carousel = document.querySelector('.carousel');
+const previousButton = document.querySelector('.previous-button');
+const nextButton = document.querySelector('.next-button');
+const contents = document.querySelector('.carousel__contents');
+
+
+nextButton.addEventListener('click', event => {
+	const currentSlide = contents.querySelector('.is-selected');
+	const nextSlide = currentSlide.nextElementSibling;
+	if (nextSlide) {
+		const destination = getComputedStyle(nextSlide).left;
+		contents.style.left = `-${destination}`;
+		currentSlide.classList.toggle('is-selected');
+		nextSlide.classList.toggle('is-selected');
+		nextSlide.nextElementSibling ? null : nextButton.setAttribute('hidden', true);
+	} 
+	previousButton.removeAttribute('hidden');
+});
+
+previousButton.addEventListener('click', event => {
+	const currentSlide = contents.querySelector('.is-selected');
+	const previousSlide = currentSlide.previousElementSibling;
+	if (previousSlide) {
+		const destination = getComputedStyle(previousSlide).left;
+		contents.style.left = `-${destination}`;
+		currentSlide.classList.toggle('is-selected');
+		previousSlide.classList.toggle('is-selected');
+		previousSlide.previousElementSibling ? null : previousButton.setAttribute('hidden', true);
+	}
+	nextButton.removeAttribute('hidden');
+});
